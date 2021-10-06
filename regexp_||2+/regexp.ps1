e630bbffa6a89ad50f4135416d459b20c7110fc6
CharlotteHonG <hunandy14@gmail.com>
2021-10-06 13:37:59 +0900

    修改成實行用的版本

--- regexp_||2+/regexp.ps1
+++ regexp_||2+/regexp.ps1
@@ -1,19 +1,30 @@
-$ct = "_Init_"
-$ct1 = "_Init_"
+[CmdletBinding()]
+Param(
+    $srcPath,
+    $dstPath
+)
 
-# 完成版
-$ct = (Get-Content 'WFWA0020.js')
+if (!$srcPath) {$srcPath = ".\js\WFWA0020.js"}
+if (!$dstPath) {
+    $dstPath = "zOut\$srcPath"
+    New-Item -ItemType File -Path $dstPath -Force | Out-Null
+}
+# ==============================================================================
+$ct = (Get-Content $srcPath)
 
 $ct_dst = $ct -match '(?<=\").*?(?=\")' -match '\|\|'
 $ct1 = $ct_dst -replace '\|\|', '\|\|' -replace '\+', '\+'
 $ct2 = $ct_dst -replace '\|\|', '+'
 
-$ct1 > out_1.js
-$ct2 > out_2.js
+$ct1 > "zOut\out_1.js"
+$ct2 > "zOut\out_2.js"
 
-# $ct1
 for ($i = 0; $i -lt $ct1.Count; $i++) {
     $ct = $ct -replace $ct1[$i], $ct2[$i]
 } $ct_out = $ct
 
-# $ct_out > out.js
+
+
+$ct2
+$ct_out > $dstPath
+# ==============================================================================
