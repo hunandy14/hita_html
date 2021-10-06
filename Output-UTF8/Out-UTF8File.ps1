51033f2f01a1d5e46de613fd0dd2a553d9a57076
CharlotteHonG <hunandy14@gmail.com>
2021-10-06 18:56:18 +0900

    Create Out-UTF8File.ps1

---
+++ Output-UTF8/Out-UTF8File.ps1
@@ -0,0 +1,9 @@
+function Out-UTF8File {
+    param (
+        $Content,
+        $FilePath = ((Get-Location).Path+"\utf8.txt")
+    )
+    $UTF8NoBomEnc = New-Object System.Text.UTF8Encoding $False
+    [System.IO.File]::WriteAllLines($FilePath, $Content, $UTF8NoBomEnc)
+}
+Out-UTF8File 'あいうえお'
