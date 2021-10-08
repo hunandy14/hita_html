d2377f1784cbd7ced03888faf1237ee9c6aa376c
CharlotteHonG <hunandy14@gmail.com>
2021-10-08 09:30:20 +0900

    Add files via upload

---
+++ cvEncode_pwsh5/cvEncode_pwsh5.ps1
@@ -0,0 +1,22 @@
+function cvUTF2JIS {
+    param (
+        $scrPath,
+        $dstPath
+    )
+    $Content = Get-Content $scrPath -Encoding UTF8
+    $UTF8NoBomEnc = New-Object System.Text.UTF8Encoding $False
+    New-Item -ItemType File -Path $dstPath -Force | Out-Null
+    [System.IO.File]::WriteAllLines($dstPath, $Content, $UTF8NoBomEnc)
+}
+
+function cvJIS2UTF {
+    param (
+        $scrPath,
+        $dstPath
+    )
+    $Content = Get-Content $scrPath -Encoding Default
+    New-Item -ItemType File -Path $dstPath -Force | Out-Null
+    Write-Output $Content | Out-File $dstPath -Encoding default
+}
+
+cvJIS2UTF .\JIS.md .\out2.md
