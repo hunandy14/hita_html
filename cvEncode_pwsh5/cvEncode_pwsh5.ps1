5b4da14803756ac0f27e62988a29937391feedf3
CharlotteHonG <hunandy14@gmail.com>
2021-10-13 14:09:15 +0900

    完成

--- cvEncode_pwsh5/cvEncode_pwsh5.ps1
+++ cvEncode_pwsh5/cvEncode_pwsh5.ps1
@@ -1,3 +1,4 @@
+# ==================================================================================================
 function cvJIS2UTF{
     param (
         $scrPath,
@@ -8,7 +9,6 @@ function cvJIS2UTF{
     New-Item -ItemType File -Path $dstPath -Force | Out-Null
     [System.IO.File]::WriteAllLines($dstPath, $Content, $UTF8NoBomEnc)
 }
-
 function cvUTF2JIS {
     param (
         $scrPath,
@@ -18,18 +18,35 @@ function cvUTF2JIS {
     New-Item -ItemType File -Path $dstPath -Force | Out-Null
     Write-Output $Content | Out-File $dstPath -Encoding default
 }
-# cvUTF2JIS .\UTF8.md .\out.md
-
-function getDirItem {
+function cvEncode_dir {
     param (
         [Parameter(Position=0)]
-        [String] $srcPath,
+        [String] $funcName,
+        [Parameter(Position=1)]
+        [String] $srcPath = "$pwd",
+        [Parameter(Position=2)]
+        [String] $dstPath,
         [Parameter(ParameterSetName = "Filter")]
         [System.Object] $Filter = @("*.*", "*.*")
     )
-    return (cgi $srcPath -R -I $Filter).FullName
-}
+    $srcPath = Resolve-Path $srcPath.TrimEnd('\/')
+    if (!(Test-Path $srcPath -pathtype 'Container')) {Write-Warning "srcPath is Not folder";return}
+    if (!$dstPath) {$dstPath = $srcPath.Substring($srcPath.LastIndexof("\")+1)}
+    if (!(Test-Path $dstPath)) {mkdir $dstPath}
+    $dstPath = Resolve-Path $dstPath.TrimEnd('\/')
+    if (!(Test-Path $dstPath -pathtype 'Container')) {Write-Warning "dstPath is Not folder";return}
+    if ($srcPath -eq $dstPath) {Write-Warning "srcPath と dstPath 同じ";return }
 
+    $Items = Get-ChildItem $srcPath -I $Filter -R
+    foreach ($item in $Items) {
+        $F1 = $item.FullName
+        $F2 = $dstPath + "\" + $item.FullName.Substring($srcPath.Length+1)
+        Write-Host "  From: $F1"
+        Write-Host "  └─To: $F2"
+        ($funcName + " '$F1' '$F2'") | Invoke-Expression
+    } Write-Host "[ $funcName Done. ]"
+}
+# ==================================================================================================
 function cvJIS2UTF_dir {
     param (
         [Parameter(Position=0)]
@@ -39,26 +56,8 @@ function cvJIS2UTF_dir {
         [Parameter(ParameterSetName = "Filter")]
         [System.Object] $Filter = @("*.*", "*.*")
     )
-    
-    
-    $srcPath = Resolve-Path $srcPath.TrimEnd('\/')
-    if (!(Test-Path $srcPath -pathtype 'Container')) {Write-Warning "srcPath is Not folder";return}
-    if (!$dstPath) {$dstPath = $srcPath.Substring($srcPath.LastIndexof("\")+1)}
-    if (!(Test-Path $dstPath)) {mkdir $dstPath}
-    $dstPath = Resolve-Path $dstPath.TrimEnd('\/')
-    if (!(Test-Path $dstPath -pathtype 'Container')) {Write-Warning "dstPath is Not folder";return}
-    if ($srcPath -eq $dstPath) {Write-Warning "srcPath と dstPath 同じ";return }
-
-    $Items = Get-ChildItem $srcPath -I $Filter -R
-    foreach ($item in $Items) {
-        $F1 = $item.FullName
-        $F2 = $dstPath + "\" + $item.FullName.Substring($srcPath.Length+1)
-        Write-Host "  From: $F1"
-        Write-Host "  └─To: $F2"
-        cvJIS2UTF $F1 $F2
-    }Write-Host "# Finish # [UTF -> JIS]"
+    cvEncode_dir "cvJIS2UTF" $srcPath $dstPath -Filter $Filter
 }
-
 function cvUTF2JIS_dir {
     param (
         [Parameter(Position=0)]
@@ -68,33 +67,16 @@ function cvUTF2JIS_dir {
         [Parameter(ParameterSetName = "Filter")]
         [System.Object] $Filter = @("*.*", "*.*")
     )
-    $srcPath = Resolve-Path $srcPath.TrimEnd('\/')
-    if (!(Test-Path $srcPath -pathtype 'Container')) {Write-Warning "srcPath is Not folder";return}
-    if (!$dstPath) {$dstPath = $srcPath.Substring($srcPath.LastIndexof("\")+1)}
-    if (!(Test-Path $dstPath)) {mkdir $dstPath}
-    $dstPath = Resolve-Path $dstPath.TrimEnd('\/')
-    if (!(Test-Path $dstPath -pathtype 'Container')) {Write-Warning "dstPath is Not folder";return}
-    if ($srcPath -eq $dstPath) {Write-Warning "srcPath と dstPath 同じ";return }
-
-    $Items = Get-ChildItem $srcPath -I $Filter -R
-    foreach ($item in $Items) {
-        $F1 = $item.FullName
-        $F2 = $dstPath + "\" + $item.FullName.Substring($srcPath.Length+1)
-        Write-Host "  From: $F1"
-        Write-Host "  └─To: $F2"
-        # cvUTF2JIS $F1 $F2
-        $cmd = "cvUTF2JIS" + " '$F1' '$F2'"
-        Invoke-Expression "$cmd"
-    } Write-Host "# Finish # [UTF -> JIS]"
+    cvEncode_dir "cvUTF2JIS" $srcPath $dstPath -Filter $Filter
 }
-
+# ==================================================================================================
 function Test {
-    # cvJIS2UTF "JIS.md" "out.md"
-    # cvUTF2JIS "out.md" "out2.md"
-    # cvJIS2UTF_dir "dir1" "d:/dir2/"
-    # cvUTF2JIS_dir "d:/dir2/"
-
-    cvUTF2JIS_dir "dir2" "dir3"
+    cvJIS2UTF "JIS.md" "out.md"
+    cvUTF2JIS "out.md" "out2.md"
+    cvJIS2UTF_dir "dir1" "d:/dir2/"
+    cvUTF2JIS_dir "d:/dir2/"
+    
+    # cvJIS2UTF "out.md" "out22.md"
 }
-
+# ==================================================================================================
 Test
