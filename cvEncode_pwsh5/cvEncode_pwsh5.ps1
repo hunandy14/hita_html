a9d95e9df5acae6559148aab12987adb1873cbd4
CharlotteHonG <hunandy14@gmail.com>
2021-10-13 12:10:50 +0900

    bug判斷句更新完成、準備要合成方法

--- cvEncode_pwsh5/cvEncode_pwsh5.ps1
+++ cvEncode_pwsh5/cvEncode_pwsh5.ps1
@@ -35,49 +35,66 @@ function cvJIS2UTF_dir {
         [Parameter(Position=0)]
         [String] $srcPath = "$pwd",
         [Parameter(Position=1)]
-        [String] $dstPath = "output",
+        [String] $dstPath,
         [Parameter(ParameterSetName = "Filter")]
         [System.Object] $Filter = @("*.*", "*.*")
     )
-    if (!(Test-Path $dstPath)) {mkdir $dstPath}
+    
+    
     $srcPath = Resolve-Path $srcPath.TrimEnd('\/')
+    if (!(Test-Path $srcPath -pathtype 'Container')) {Write-Warning "srcPath is Not folder";return}
+    if (!$dstPath) {$dstPath = $srcPath.Substring($srcPath.LastIndexof("\")+1)}
+    if (!(Test-Path $dstPath)) {mkdir $dstPath}
     $dstPath = Resolve-Path $dstPath.TrimEnd('\/')
+    if (!(Test-Path $dstPath -pathtype 'Container')) {Write-Warning "dstPath is Not folder";return}
+    if ($srcPath -eq $dstPath) {Write-Warning "srcPath と dstPath 同じ";return }
+
     $Items = Get-ChildItem $srcPath -I $Filter -R
     foreach ($item in $Items) {
         $F1 = $item.FullName
         $F2 = $dstPath + "\" + $item.FullName.Substring($srcPath.Length+1)
-        Write-Warning "  From: $F1"
-        Write-Warning "  └─To: $F2"
+        Write-Host "  From: $F1"
+        Write-Host "  └─To: $F2"
         cvJIS2UTF $F1 $F2
-    }
-    Write-Warning "# Finish # [UTF -> JIS]"
+    }Write-Host "# Finish # [UTF -> JIS]"
 }
+
 function cvUTF2JIS_dir {
     param (
         [Parameter(Position=0)]
         [String] $srcPath = "$pwd",
         [Parameter(Position=1)]
-        [String] $dstPath = "output",
+        [String] $dstPath,
         [Parameter(ParameterSetName = "Filter")]
         [System.Object] $Filter = @("*.*", "*.*")
     )
-    if (!(Test-Path $dstPath)) {mkdir $dstPath}
     $srcPath = Resolve-Path $srcPath.TrimEnd('\/')
+    if (!(Test-Path $srcPath -pathtype 'Container')) {Write-Warning "srcPath is Not folder";return}
+    if (!$dstPath) {$dstPath = $srcPath.Substring($srcPath.LastIndexof("\")+1)}
+    if (!(Test-Path $dstPath)) {mkdir $dstPath}
     $dstPath = Resolve-Path $dstPath.TrimEnd('\/')
-    $srcPath
+    if (!(Test-Path $dstPath -pathtype 'Container')) {Write-Warning "dstPath is Not folder";return}
+    if ($srcPath -eq $dstPath) {Write-Warning "srcPath と dstPath 同じ";return }
+
     $Items = Get-ChildItem $srcPath -I $Filter -R
     foreach ($item in $Items) {
         $F1 = $item.FullName
         $F2 = $dstPath + "\" + $item.FullName.Substring($srcPath.Length+1)
-        Write-Warning "  From: $F1"
-        Write-Warning "  └─To: $F2"
-        cvUTF2JIS $F1 $F2
-    }
-    Write-Warning "# Finish # [UTF -> JIS]"
+        Write-Host "  From: $F1"
+        Write-Host "  └─To: $F2"
+        # cvUTF2JIS $F1 $F2
+        $cmd = "cvUTF2JIS" + " '$F1' '$F2'"
+        Invoke-Expression "$cmd"
+    } Write-Host "# Finish # [UTF -> JIS]"
 }
 
+function Test {
+    # cvJIS2UTF "JIS.md" "out.md"
+    # cvUTF2JIS "out.md" "out2.md"
+    # cvJIS2UTF_dir "dir1" "d:/dir2/"
+    # cvUTF2JIS_dir "d:/dir2/"
 
-cvJIS2UTF "JIS.md" "out.md"
-cvUTF2JIS "out.md" "out2.md"
-cvJIS2UTF_dir "dir1" "d:/dir2/"
-cvUTF2JIS_dir "d:/dir2/"
+    cvUTF2JIS_dir "dir2" "dir3"
+}
+
+Test
