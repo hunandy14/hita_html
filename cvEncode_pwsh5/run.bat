b1f8ae658a7de0fabb395c47014028d2b681a03b
CharlotteHonG <hunandy14@gmail.com>
2021-10-14 17:23:27 +0900

    Update run.bat

--- cvEncode_pwsh5/run.bat
+++ cvEncode_pwsh5/run.bat
@@ -1,1 +1,25 @@
-pwsh -Command "& {.\cvEncode.ps1 }"
+echo off
+@REM pwsh -Command "& {.\cvEncode_pwsh5.ps1 'cvUTF2JIS_dir' 'PATH1' 'PATH2'}"
+
+SET funcName[0]=cvJIS2UTF
+SET funcName[1]=cvUTF2JIS
+SET funcName[2]=cvJIS2UTF_dir
+SET funcName[3]=cvUTF2JIS_dir
+
+
+SET funcp=%funcName[2]%
+SET srcPath=D:\Users\71856726\OneDrive - Hitachi Group\HitachiXPC\Desktop\ap\gitRepo\hita_html
+SET dstPath=
+SET srcFile=JIS.md
+SET dstFile=
+
+
+
+@REM SET parm='%funcName[2]%' '%srcPath%' '%dstPath%'
+SET parm='%funcName[2]%' '%srcPath%' '%dstPath%' -Filter *.md
+
+@REM SET parm='%funcName[2]%' '%srcPath%' '%dstPath%'
+@REM SET parm='%funcName[2]%' '%srcPath%' '%dstPath%'
+
+@REM echo %parm%
+POWERSHELL -Command "& {.\cvEncode_pwsh5.ps1 %parm%}"
