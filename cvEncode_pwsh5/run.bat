4458596294530086c7a6832a48cee359c61732f2
CharlotteHonG <hunandy14@gmail.com>
2021-10-15 10:00:25 +0900

    Update run.bat

--- cvEncode_pwsh5/run.bat
+++ cvEncode_pwsh5/run.bat
@@ -1,25 +1,23 @@
 echo off
-@REM pwsh -Command "& {.\cvEncode_pwsh5.ps1 'cvUTF2JIS_dir' 'PATH1' 'PATH2'}"
-
+::関数
 SET funcName[0]=cvJIS2UTF
 SET funcName[1]=cvUTF2JIS
 SET funcName[2]=cvJIS2UTF_dir
 SET funcName[3]=cvUTF2JIS_dir
-
-
-SET funcp=%funcName[2]%
+::設定値
 SET srcPath=D:\Users\71856726\OneDrive - Hitachi Group\HitachiXPC\Desktop\ap\gitRepo\hita_html
-SET dstPath=
+SET dstPath=OUTPUT
 SET srcFile=JIS.md
-SET dstFile=
+SET dstFile=OUTPUT.md
 
 
+::使いの例
+@REM SET parm=%funcName[0]% '%srcFile%' '%dstFile%'
+@REM SET parm=%funcName[2]% '%srcPath%' -Filter *.md
+@REM SET parm=%funcName[2]% '%srcPath%' '%dstPath%'
+@REM SET parm=%funcName[2]% '%srcPath%' '%dstPath%' -Filter *.md
 
-@REM SET parm='%funcName[2]%' '%srcPath%' '%dstPath%'
-SET parm='%funcName[2]%' '%srcPath%' '%dstPath%' -Filter *.md
 
-@REM SET parm='%funcName[2]%' '%srcPath%' '%dstPath%'
-@REM SET parm='%funcName[2]%' '%srcPath%' '%dstPath%'
-
-@REM echo %parm%
+::プログラム開始
+:: echo %parm%
 POWERSHELL -Command "& {.\cvEncode_pwsh5.ps1 %parm%}"
