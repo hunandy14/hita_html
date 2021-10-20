e12de1d5b882ebffd0b886c4c7ced457086f0d4e
CharlotteHonG <hunandy14@gmail.com>
2021-10-20 16:08:58 +0900

    Update regexp.ps1

--- regexp_||2+/regexp.ps1
+++ regexp_||2+/regexp.ps1
@@ -4,7 +4,7 @@ Param(
     $dstPath
 )
 
-if (!$srcPath) {$srcPath = ".\js\WFWA0020.js"}
+if (!$srcPath) {$srcPath = ".\js\WFWD0200.js"}
 if (!$dstPath) {
     $dstPath = "zOut\$srcPath"
     New-Item -ItemType File -Path $dstPath -Force | Out-Null
@@ -13,11 +13,23 @@ if (!$dstPath) {
 $ct = (Get-Content $srcPath)
 
 $ct_dst = $ct -match '(?<=\").*?(?=\")' -match '\|\|'
-$ct1 = $ct_dst -replace '\|\|', '\|\|' -replace '\+', '\+'
+
+$ct1 = $ct_dst -replace '\|\|', '\|\|' `
+            -replace '\(', '\(' `
+            -replace '\)', '\)' `
+            -replace '\[', '\[' `
+            -replace '\]', '\]' `
+            -replace '\{', '\}' `
+            -replace '\{', '\}' `
+            -replace '\+', '\+' `
+            -replace '\-', '\-' `
+            -replace '\*', '\*' `
+            -replace '\/', '\/'
+
 $ct2 = $ct_dst -replace '\|\|', '+'
 
-$ct1 > "zOut\out_1.js"
-$ct2 > "zOut\out_2.js"
+# $ct1 > "zOut\out_1.js"
+# $ct2 > "zOut\out_2.js"
 
 for ($i = 0; $i -lt $ct1.Count; $i++) {
     $ct = $ct -replace $ct1[$i], $ct2[$i]
@@ -26,5 +38,5 @@ for ($i = 0; $i -lt $ct1.Count; $i++) {
 
 
 $ct2
-$ct_out > $dstPath
+$ct_out | Out-File $dstPath -Encoding default
 # ==============================================================================
