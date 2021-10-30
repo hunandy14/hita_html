839832795a0f025500727a3965934cb04ba0bb5e
CharlotteHonG <hunandy14@gmail.com>
2021-10-30 23:21:15 +0900

    Update Start_ProtablePython.ps1

--- ProtablePthon/Start_ProtablePython.ps1
+++ ProtablePthon/Start_ProtablePython.ps1
@@ -1,7 +1,12 @@
 $path=$PSScriptRoot
 
 $bin=$path+"\Portable Python-3.9.4 x64\App\Python"
-$env:path= $env:path+";$bin;"
-
 $bin2=$path+"\Portable Python-3.9.4 x64\App\Python\Scripts"
-$env:path= $env:path+";$bin2;"
+$env:path= $env:path+";$bin;$bin2"
+
+# reintall pip
+python -m pip install --upgrade --force-reinstall pip
+
+# Test Numpy
+"import numpy as np; print(np.array(['Hello', 'Numpy']));" > TestNumpy.py
+python .\TestNumpy.py
