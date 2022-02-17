9afe831a67753a7b953201431bb7f99de3ab912d
CharlotteHonG <hunandy14@gmail.com>
2022-02-17 16:31:59 +0900

    createElementFromHTML

---
+++ createElementFromHTML/createElementFromHTML.js
@@ -0,0 +1,8 @@
+// http://vigges.net/?qa=398452/
+function createElementFromHTML(htmlString) {
+  var div = document.createElement('div');
+  div.innerHTML = htmlString.trim();
+
+  // Change this to div.childNodes to support multiple top-level nodes
+  return div.firstChild; 
+}
