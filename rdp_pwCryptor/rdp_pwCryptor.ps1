cd04f96361c1227ed8f0d02376ba796ab27cf259
CharlotteHonG <hunandy14@gmail.com>
2022-02-18 22:16:26 +0900

    遠端加密

---
+++ rdp_pwCryptor/rdp_pwCryptor.ps1
@@ -0,0 +1,33 @@
+function decryptor {
+    Param(
+    [Parameter(Mandatory=$true)]
+    [string]$PasswordEncryptedAsHex
+    )
+
+    Add-Type -AssemblyName System.Security
+
+    [System.Text.UnicodeEncoding]$encoding = [System.Text.Encoding]::Unicode
+    [int]$passwordEncryptedAsBytesLength = $PasswordEncryptedAsHex.Length / 2
+    [byte[]]$passwordEncryptedAsBytes = New-Object -TypeName byte[] -ArgumentList $passwordEncryptedAsBytesLength
+    for ($i = 0; $i -lt $passwordEncryptedAsBytesLength; $i++) {
+        $passwordEncryptedAsBytes[$i] = [Convert]::ToByte($PasswordEncryptedAsHex.Substring($i * 2, 2), 16)
+    }
+    [byte[]]$passwordAsBytes = [System.Security.Cryptography.ProtectedData]::Unprotect($passwordEncryptedAsBytes, $null, [System.Security.Cryptography.DataProtectionScope]::CurrentUser)
+    [string]$password = $encoding.GetString($passwordAsBytes)
+    return $password
+}
+
+function encryptor {
+    Param(
+        [Parameter(Mandatory=$true)]
+        [string]$Password
+    )
+
+    Add-Type -AssemblyName System.Security
+
+    [System.Text.UnicodeEncoding]$encoding = [System.Text.Encoding]::Unicode
+    [byte[]]$passwordAsBytes = $encoding.GetBytes($Password)
+    [byte[]]$passwordEncryptedAsBytes = [System.Security.Cryptography.ProtectedData]::Protect($passwordAsBytes, $null, [System.Security.Cryptography.DataProtectionScope]::CurrentUser)
+    [string]$passwordEncryptedAsHex = -join ($passwordEncryptedAsBytes | ForEach-Object { $_.ToString("X2") })
+    return $passwordEncryptedAsHex
+}
