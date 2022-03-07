57127c3cf9338d13e58fe8e9ae4be952033ef5f6
CharlotteHonG <hunandy14@gmail.com>
2022-03-07 13:12:50 +0900

    Create edge_IE.js

---
+++ edge_IE/edge_IE.js
@@ -0,0 +1,7 @@
+function SF_SEWA0007_ExecApplication(){
+	if (window.navigator.userAgent.indexOf('MSIE') > 0) {
+		SF_SEWA0007_ExecApplication_IE()
+	} else {
+		SF_SEWA0007_ExecApplication_Edge()
+	}
+}
