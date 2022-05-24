203f8f403bab34961a248245176200c109c0c9c7
CharlotteHonG <hunandy14@gmail.com>
2022-05-24 18:44:01 +0900

    Add files via upload

---
+++ 5. batch/戻り値のテスト/EXPZ10.bat
@@ -0,0 +1,16 @@
+@echo off
+ 
+rem コマンドプロンプト画面へのメッセージの出力
+echo ここは子バッチの中です。
+
+@REM call EXPZ10\0.bat
+call EXPZ10\1.bat
+echo %errorlevel%
+
+
+rem 処理を終了する
+@REM exit
+
+if %errorlevel%==0 (echo "処理成功") else (echo "処理失敗")
+
+exit /b %errorlevel%
