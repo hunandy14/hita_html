a047bc29e9c913fcda7a4399108919056bec215d
CharlotteHonG <hunandy14@gmail.com>
2022-06-27 21:51:25 +0900

    Update StartBoxCli.bat

--- 5. batch/StartBoxCli/StartBoxCli.bat
+++ 5. batch/StartBoxCli/StartBoxCli.bat
@@ -1,6 +1,6 @@
 Set BoxPath=.\
-Set WorkPath=[Environment]::GetFolderPath('Desktop')
+Set WorkPath=$([System.Environment]::GetFolderPath([System.Environment+SpecialFolder]::Desktop))
 Set Msg=$(box --version)`n
 
 Set Cmd1=$_=[System.IO.Path]::GetFullPath('%BoxPath%')+'bin\box.cmd'; Set-Alias box $_
-start cmd /c PowerShell.exe -NoExit "%Cmd1%; Write-Host %Msg% -Fore:Y; Set-Location $(%WorkPath%);"
+start cmd /c PowerShell.exe -NoExit "%Cmd1%; Write-Host %Msg% -Fore:Y; $s='%WorkPath%'; if($s[0] -eq '$'){Set-Location ($s|iex)}else{Set-Location $s};"
