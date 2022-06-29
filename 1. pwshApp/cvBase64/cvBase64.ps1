35b1ee33a80952327f388c5cda8d6d50a00cb0de
CharlotteHonG <hunandy14@gmail.com>
2022-06-29 17:34:02 +0900

    Update cvBase64.ps1

--- 1. pwshApp/cvBase64/cvBase64.ps1
+++ 1. pwshApp/cvBase64/cvBase64.ps1
@@ -1,7 +1,7 @@
-function deBase64($String) { 
-    return [System.Convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes($String)) 
-} # deBase64 "Charlotte"
+function deBase64($String) {
+    return [System.Convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes($String))
+} # deBase64 "SampleString"
 
-function enBase64($Code) { 
-    return [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String($code)) 
-} # enBase64 "Q2hhcmxvdHRl"
+function enBase64($Code) {
+    return [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String($code))
+} # enBase64 "U2FtcGxlU3RyaW5n"
